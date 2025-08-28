ae169b480d1f75cd0f1f70cc9d6016be22dfbdb6
Jay Danagashiya <pjay22756@gmail.com>
2025-08-28 09:40:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 20;
+
+
+console.log("Addition of a and b = " + (a + b));
+
+console.log("Subtraction of a and b = " + (a - b));
+
+console.log("Multiplication of a and b = " + (a * b));
+
+console.log("Modulus of a and b = " + (a % b));
+
+console.log("Division of a and b = " + (a / b));
+
+console.log("Increment of a and b (b+1) = " + (a + (b + 1)));
+
+console.log("Exponentiation of a and b = " + (a ** b));
+
+console.log("Decrement of a and b (b-1) = " + (a + (b - 1)));
